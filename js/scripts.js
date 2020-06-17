360d7ac5d76afadb9e8762f6a6ac60df099a9cd7
abauruel <abauruel@gmail.com>
2020-06-17 13:29:02 -0300

    code sanitized

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,36 @@
 let score = 0;
-function choose(e) {
-  let housePicked;
-  const arrayOptions = ["rock", "paper", "scissors"];
+const arrayOptions = ["rock", "paper", "scissors"];
+let housePicked;
 
+function rules(result, namechoisen) {
+  const scoreElement = document
+    .getElementsByClassName("header-game-score")[0]
+    .getElementsByTagName("p")[0];
+  let message;
+  const myNumber = arrayOptions.findIndex((name) => name === namechoisen);
+  const homeNumber = arrayOptions.findIndex((name) => name === result);
+  if (myNumber == homeNumber) {
+    message = "You Tie";
+  } else if (
+    (myNumber - homeNumber) % 3 == 1 ||
+    (myNumber - homeNumber) % 3 == -2
+  ) {
+    message = "you win";
+    score++;
+    scoreElement.textContent = score;
+  } else {
+    message = "you lose";
+  }
+
+  document.getElementById("resultText").textContent = message;
+  document.getElementById("result").style.display = "flex";
+}
+
+function Aleatory() {
+  return arrayOptions[Math.floor(Math.random() * arrayOptions.length)];
+}
+
+function choice(e) {
   //check button close clicked
   if (this.id === "close") {
     console.log("close");
@@ -10,59 +38,41 @@ function choose(e) {
     return;
   }
 
-  //check button playagain clicked
+  //check button playagain it was clicked
   if (this.id === "playagain") {
+    //hide and show containers
     document.getElementById("selectors").style.display = "flex";
     document.getElementsByClassName("game")[0].style.display = "none";
     document.getElementById("result").style.display = "none";
     return;
   }
+
+  //check button RULES it was clicked
   if (this.id === "rules") {
-    console.log("rules");
-    console.log(
-      (document.getElementsByClassName("rules")[0].style.display = "flex")
-    );
+    //show modal
+    document.getElementsByClassName("rules")[0].style.display = "flex";
     return;
   }
+
+  //hide container chosen
   document.getElementsByClassName("game")[0].style.display = "flex";
-  const containerselection = document.getElementById("selectors"); //container de selecao
-  const mychoose = document.getElementById("choose"); //elemento escolhido
-  const housePickedElement = document.getElementById("opponent"); //elemento do oponente
-  const scoreElement = document
-    .getElementsByClassName("header-game-score")[0]
-    .getElementsByTagName("p")[0];
 
-  const namechoose = this.dataset.namechoose;
-  function rules(result) {
-    let message;
-    const myNumber = arrayOptions.findIndex((name) => name === namechoose);
-    const homeNumber = arrayOptions.findIndex((name) => name === result);
-    if (myNumber == homeNumber) {
-      message = "You Tie";
-    } else if ((myNumber - homeNumber) % 3 == 1) {
-      message = "you win";
-      score++;
-      scoreElement.textContent = score;
-    } else {
-      message = "you lose";
-    }
-    document.getElementById("resultText").textContent = message;
-    document.getElementById("result").style.display = "flex";
-  }
-  //elemento escolhido
-  const choosedName = this.dataset.namechoose;
+  const containerselection = document.getElementById("selectors");
+  const mychoice = document.getElementById("choice");
 
-  //set image on choosed
-  const sourceImage = mychoose.getElementsByTagName("img");
+  //get data name
+  const choisenName = this.dataset.namechoice;
+
+  //close container choisen
+  containerselection.style.display = "none";
+  const sourceImage = mychoice.getElementsByTagName("img");
   sourceImage[0].src = this.getElementsByTagName("img")[0].src;
 
-  //close container choosed
-  containerselection.style.display = "none";
-  function Aleatory() {
-    return arrayOptions[Math.floor(Math.random() * arrayOptions.length)];
-  }
-  console.log(housePickedElement);
-  function roleta() {
+  function roulette() {
+    const housePickedElement = document.getElementById("opponent"); //elemento do oponente
+
+    //set image on choisen
+
     housePicked = Aleatory();
     if (!housePickedElement.className) {
       housePickedElement.classList.add("button-" + housePicked);
@@ -71,27 +81,27 @@ function choose(e) {
       housePickedElement.classList.add("button-" + housePicked);
     }
     const srcImgHousePicked = sourceImage[0].src.replace(
-      choosedName,
+      choisenName,
       housePicked
     );
     housePickedElement.getElementsByTagName("img")[0].src = srcImgHousePicked;
   }
 
-  //escolha aleatoria
-  var startRoleta = setInterval(roleta, 200);
+  //random choice
+  const startRoulette = setInterval(roulette, 200);
   setTimeout(() => {
-    clearInterval(startRoleta);
-    rules(housePicked);
+    clearInterval(startRoulette);
+    rules(housePicked, choisenName);
   }, 3000);
-
-  if (!mychoose.className) {
-    mychoose.classList.add("button-" + this.dataset.namechoose);
+  console.log(choisenName);
+  if (!mychoice.className) {
+    mychoice.classList.add("button-" + choisenName);
   } else {
-    mychoose.classList.remove(mychoose.className);
-    mychoose.classList.add("button-" + choosedName);
+    mychoice.classList.remove(mychoice.className);
+    mychoice.classList.add("button-" + choisenName);
   }
 }
 
 const buttons = document.querySelectorAll("button");
 
-buttons.forEach((button) => button.addEventListener("click", choose));
+buttons.forEach((button) => button.addEventListener("click", choice));
